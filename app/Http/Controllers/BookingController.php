e10a5d8b64ef9f16e86312a9409bd2af34d9a118
aliahmadcse <aliahmadcse@gmail.com>
2020-04-17 02:59:00 +0500

    Adds action for creating booking

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -26,7 +26,12 @@ class BookingController extends Controller
      */
     public function create()
     {
-        //
+        $users=DB::table('users')->get()->pluck('name','id')
+                ->prepend('none');
+        $rooms=DB::table('rooms')->get()->pluck('number','id');
+        return view('bookings.create')
+                ->with('users',$users)
+                ->with('rooms',$rooms);
     }
 
     /**
