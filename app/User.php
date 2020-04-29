025eac7c06cfaacf4e12e623132e2082aee15953
aliahmadcse <aliahmadcse@gmail.com>
2020-04-29 19:13:32 +0500

    Revert "Revert "Updates the version to the latest""
    This reverts commit b51e99ac9ccad72659e54505fcc973b02ec5b96e.

--- app/User.php
+++ app/User.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -36,10 +36,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
-    public function bookings()
-    {
-        return $this->belongsToMany('App\Booking','bookings_users',
-        'user_id','booking_id')->withTimestamps();
-    }
 }
