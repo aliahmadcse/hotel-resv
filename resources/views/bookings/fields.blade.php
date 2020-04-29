fde35f022f94666988f6e5fc7f7d62c2c89b619a
aliahmadcse <aliahmadcse@gmail.com>
2020-04-30 02:58:40 +0500

    Adds error displays and retrieves old fields

--- resources/views/bookings/fields.blade.php
+++ resources/views/bookings/fields.blade.php
@@ -1,3 +1,11 @@
+@if ($errors->any())
+@foreach ($errors->all() as $error)
+<p class="alert alert-danger">
+    {{ $error }}
+</p>
+@endforeach
+@endif
+
 <div class="form-group row">
     <label class="col-sm-2 col-form-label" for="room_id">Room</label>
     <div class="col-sm-10">
@@ -17,7 +25,8 @@
         <select name="user_id" class="form-control" id="user_id" required>
             @foreach($users as $id => $display)
             <option value="{{ $id }}"
-                {{ (isset($bookingsUser->user_id) && $id === $bookingsUser->user_id) ? 'selected' : '' }}>{{ $display }}
+                {{ old('user_id') ?? (isset($bookingsUser->user_id) && $id === $bookingsUser->user_id) ? 'selected' : '' }}>
+                {{ $display }}
             </option>
             @endforeach
         </select>
@@ -29,7 +38,7 @@
     <label class="col-sm-2 col-form-label" for="start">Start Date</label>
     <div class="col-sm-10">
         <input name="start" type="date" class="form-control" required placeholder="yyyy-mm-dd"
-            value="{{ $booking->start ?? '' }}" />
+            value="{{ old('start') ?? $booking->start ?? '' }}" />
         <small class="form-text text-muted">The start date for the booking.</small>
     </div>
 </div>
@@ -38,7 +47,7 @@
     <label class="col-sm-2 col-form-label" for="start">End Date</label>
     <div class="col-sm-10">
         <input name="end" type="date" class="form-control" required placeholder="yyyy-mm-dd"
-            value="{{ $booking->end ?? '' }}" />
+            value="{{ old('end') ?? $booking->end ?? '' }}" />
         <small class="form-text text-muted">The end date for the booking.</small>
     </div>
 </div>
@@ -48,7 +57,7 @@
     <div class="col-sm-10">
         <div class="form-check">
             <input name="is_paid" type="checkbox" class="form-check-input" value="1"
-                {{ (isset($booking->is_paid) && $booking->is_paid===1) ? 'checked' : '' }} />
+                {{ old('is_paid') ?? (isset($booking->is_paid) && $booking->is_paid===1) ? 'checked' : '' }} />
             <label class="form-check-label" for="start">Pre-Paid</label>
             <small class="form-text text-muted">If the booking is being pre-paid.</small>
         </div>
@@ -58,7 +67,8 @@
 <div class="form-group row">
     <label class="col-sm-2 col-form-label" for="notes">Notes</label>
     <div class="col-sm-10">
-        <input name="notes" type="text" class="form-control" placeholder="Notes" value="{{ $booking->notes ?? '' }}" />
+        <input name="notes" type="text" class="form-control" placeholder="Notes"
+            value="{{ old('notes') ??  $booking->notes ?? '' }}" />
         <small class="form-text text-muted">Any notes for the booking.</small>
     </div>
 </div>
