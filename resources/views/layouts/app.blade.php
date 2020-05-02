def228500f4bab9060ce2cd85d611271efcf43ae
aliahmadcse <aliahmadcse@gmail.com>
2020-05-03 03:19:13 +0500

    Fixes the test change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -179,7 +179,7 @@
       </ul>
       <form class="form-inline mt-2 mt-md-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Searc</button>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
     </div>
   </nav>
