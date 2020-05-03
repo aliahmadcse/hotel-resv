2f3235cb162b737f7f170730c3e4481eba6a3fdc
aliahmadcse <aliahmadcse@gmail.com>
2020-05-03 19:53:34 +0500

    Updates database to azure

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,11 +43,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => 'aliahmad-mysql.mysql.database.azure.com',
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'hotel-resv',
+            'username' => 'aliahmaddev@aliahmad-mysql',
+            'password' => 'Aliahmad244623',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
