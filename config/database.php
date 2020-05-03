14a68b723981d3423c782ccca356cf640ea3f6f7
aliahmadcse <aliahmadcse@gmail.com>
2020-05-03 20:15:10 +0500

    Revert "Updates database to azure"
    This reverts commit 2f3235cb162b737f7f170730c3e4481eba6a3fdc.

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,11 +43,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'aliahmad-mysql.mysql.database.azure.com',
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'hotel-resv',
-            'username' => 'aliahmaddev@aliahmad-mysql',
-            'password' => 'Aliahmad244623',
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
